6fc349565265ddbc0a1d2650a1e271969266f955
Pedro Figueiredo <pedro@neon.tech>
2024-12-03 13:10:23 +0000

    feat: prepare for publish

--- build/state.js
+++
@@ -1,7 +0,0 @@
-const migrationsState = new Map();
-export function getMigrationFromMemory(migrationId) {
-    return migrationsState.get(migrationId);
-}
-export function persistMigrationToMemory(migrationId, migrationDetails) {
-    migrationsState.set(migrationId, migrationDetails);
-}
